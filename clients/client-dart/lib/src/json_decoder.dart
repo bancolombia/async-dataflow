6c0b4fc0167017bdb3990efad6ce2384e9308494
Gabriel Martinez <gabrieljm@gmail.com>
2022-04-04 19:16:18 -0500

    solution for https://github.com/bancolombia/async-dataflow/issues/17

--- clients/client-dart/lib/src/json_decoder.dart
+++ clients/client-dart/lib/src/json_decoder.dart
@@ -1,15 +1,18 @@
 import 'dart:convert';
-import 'package:validators/validators.dart';
+import 'package:logging/logging.dart';
 import 'channel_message.dart';
 import 'message_decoder.dart';
 import 'utils.dart';
+
 class JsonDecoder extends MessageDecoder<String> {
+
+  final _log = Logger('AsyncClient');
 
   @override
   ChannelMessage decode(String event) {
-
-    var event_as_list = _tokenize(_removeKeys(event));
     
+    var event_as_list = jsonDecode('{"received": $event }')['received'];
+
     var msg = ChannelMessage(Utils.checkString(event_as_list[0]), 
       Utils.checkString(event_as_list[1]),
       Utils.checkString(event_as_list[2]),
@@ -18,28 +21,15 @@ class JsonDecoder extends MessageDecoder<String> {
     var data = Utils.checkString(event_as_list[3]);
 
     if (data != null) {
-      if (isJSON(data)) {
+      try {
         msg.payload = jsonDecode(data);
-      } else {
+      } catch (e) {
+        _log.warning('payload its not decodeable to json');
         msg.payload = data;
       }
     }
 
     return msg;
-  }
-
-  String _removeKeys(String event) {
-    return event.replaceAll(RegExp(r'(^\[)'), '')
-      .replaceAll(RegExp(r'(]$)'), '');
-  }
-
-  List<String> _tokenize(String event) {
-    return event.split(RegExp(r',(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)'))
-      .map((e) => e.trim())
-      .map((e) => e.replaceAll(RegExp(r'["]{2}'), ''))
-      .map((e) => e.replaceAll(RegExp(r'^"'), ''))
-      .map((e) => e.replaceAll(RegExp(r'"$'), ''))
-      .toList();
   }
 
 }
