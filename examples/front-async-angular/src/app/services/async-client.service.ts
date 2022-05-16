2db0fb5a6bd4c1e089f8c1bbe91f6efbe71786ab
Santiago Garcia Gil <sagarci@bancolombia.com.co>
2022-05-16 10:36:04 -0500

    update example chanjs

--- examples/front-async-angular/src/app/services/async-client.service.ts
+++ examples/front-async-angular/src/app/services/async-client.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AsyncClient } from 'chanjs-client';
+import { AsyncClient } from '@bancolombia/chanjs-client';
 import { Subject } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Message } from '../models/message.inteface';
