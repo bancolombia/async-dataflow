fd037f4dc3a03afa7ef4f75f2e7d468a2911ab43
Gabriel Martinez <gabrieljm@gmail.com>
2025-05-14 21:53:50 -0500

    fix: inc version

--- channel-sender/mix.exs
+++ channel-sender/mix.exs
@@ -4,7 +4,7 @@ defmodule ChannelSenderEx.MixProject do
   def project do
     [
       app: :channel_sender_ex,
-      version: "0.2.3",
+      version: "0.2.4",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
