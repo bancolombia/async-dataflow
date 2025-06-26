ebfb155cdbe517dc374bff7f1131898f5e384466
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2025-06-26 10:56:23 -0500

    Inject opentelemetry

--- clients/backend-client-elixir/mix.exs
+++ clients/backend-client-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule AdfSenderConnector.MixProject do
   def project do
     [
       app: :adf_sender_connector,
-      version: "0.4.1",
+      version: "0.4.0",
       elixir: "~> 1.16",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
