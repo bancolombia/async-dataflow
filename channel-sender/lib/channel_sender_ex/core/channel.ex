218fa83a50341337e78a869087044250b0516190
SOFKA\jhusseth.sanchez <jhvasanc@bancolombia.com.co>
2025-07-14 07:53:45 -0500

    fix issue for credo analisys

--- channel-sender/lib/channel_sender_ex/core/channel.ex
+++ channel-sender/lib/channel_sender_ex/core/channel.ex
@@ -5,7 +5,7 @@ defmodule ChannelSenderEx.Core.Channel do
   Main abstraction for modeling and active or temporarily idle async communication channel with an user.
   """
   use GenStateMachine, callback_mode: [:state_functions, :state_enter], restart: :transient
-  require OpenTelemetry.Tracer, as :Tracer
+  require OpenTelemetry.Tracer, as: Tracer
   require Logger
   alias ChannelSenderEx.Core.BoundedMap
   alias ChannelSenderEx.Core.ChannelIDGenerator
