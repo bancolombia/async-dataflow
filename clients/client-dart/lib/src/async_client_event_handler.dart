cc0137757a80892e68d270282f35f4532c05eeee
Juan Campuzano <diegocampuzano62@gmail.com>
2025-07-17 11:11:30 -0500

    feat: add event handler for sse connection

--- clients/client-dart/lib/src/async_client_event_handler.dart
+++ clients/client-dart/lib/src/async_client_event_handler.dart
@@ -1,13 +1,17 @@
+import '../channel_sender_client.dart';
+
 abstract class AsyncClientEventHandler {
   void onEvent(AsyncClientEvent event);
 }
 
 class AsyncClientEvent {
   final String message;
+  final TransportType transportType;
   final String? channelRef;
 
   AsyncClientEvent({
     required this.message,
+    this.transportType = TransportType.ws,
     this.channelRef,
   });
 }
