c4a99f7e15c224c76e47c13c5aaff223b72c5ac8
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2025-11-14 16:41:48 -0500

    test(deps): arrange mods

--- channel-sender/test/channel_sender_ex/utils/custom_telemetry_test.exs
+++ channel-sender/test/channel_sender_ex/utils/custom_telemetry_test.exs
@@ -73,8 +73,7 @@ defmodule ChannelSenderEx.Utils.CustomTelemetryTest do
         }
 
         case CustomTelemetry.start_span(:test, req, "ch_123") do
-          {:span_ctx, _trace_id, _span_id, _parent_span_id, _tracestate, _sampled, _ended,
-           _remote, _name} ->
+          {:span_ctx, _trace_id, _span_id, _parent_span_id, _, _tracestate, _sampled, _ended, _remote, _name} ->
             assert true
 
           other ->
