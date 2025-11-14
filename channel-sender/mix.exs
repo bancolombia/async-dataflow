8faab525cc7f428440f1bb3fcee6d2cd418a9f8e
Juan C Galvis <8420868+juancgalvis@users.noreply.github.com>
2025-11-14 16:03:26 -0500

    fix(deps): update version

--- channel-sender/mix.exs
+++ channel-sender/mix.exs
@@ -4,7 +4,7 @@ defmodule ChannelSenderEx.MixProject do
   def project do
     [
       app: :channel_sender_ex,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
