06c457289a86b232cc837f44ed60faea68056e5d
Diana Suasnavas <diana.suasnavas@thoughtworks.com>
2026-05-06 14:36:50 -0500

    refactor(clients): change constructor AsyncClient to have an object

--- clients/client-js/src/index.ts
+++ clients/client-js/src/index.ts
@@ -1,3 +1,3 @@
 export { AsyncClient } from './async-client'
-export { AsyncConfig } from './async-config'
+export { AsyncConfig, AsyncClientOptions } from './async-config'
 export { ChannelMessage } from './channel-message'
